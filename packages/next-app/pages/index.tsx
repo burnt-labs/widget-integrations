17ac41ae8121ada2369661a3ddf163552d47ea09
peartes <kenny.fale.kf@gmail.com>
2024-01-10 17:19:59 +0100

    chore: xion app

--- packages/next-app/pages/index.tsx
+++ packages/next-app/pages/index.tsx
@@ -6,7 +6,7 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   const config = {
     companyName: "Test Widget",
-    integratorId: "example-swap-widget",
+    integratorId: "xion-swap-widget",
     slippage: 3,
     instantExec: true,
     infiniteApproval: false,
